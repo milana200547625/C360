40aa42f583fdccfce8049192302fb3e743f2b3e6
milana200547625 <milanarebecca.isaacs@mygeorgian.ca>
2023-08-04 01:04:48 -0400

    C360

---
+++ Contact360/Contact360/Controllers/ContactController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contact360.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Contact360/Contact360/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Contact360.Models;
+
+namespace Contact360.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Contact360.Models.Contact> Contact { get; set; } = default!;
+    }
+}
